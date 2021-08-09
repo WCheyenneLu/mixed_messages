ec96146270cba8b89b2b5acbd2e0123242566d37
Cheyenne Lu <cheyennelu17@gmail.com>
2021-08-09 13:40:16 +0700

    adding switch 3 case switch

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,1 +1,15 @@
-/*mixed_messages.js*/
+let randomNumber = Math.floor(Math.random()*3);
+let joke = '';
+swtich (randomNumber); { 
+    case 0 : 
+    joke = 'Why did Stalin cross the road? .... because he was Russi\'n';
+    break;
+    case 1 : 
+    joke = 'Today an old lady asked me to check her balance.... so I pushed her over';
+    break;
+    case 2 : 
+    joke = 'What did the shark say when he ate the clownfish...this tastes a little funny';
+    break;
+    default: 
+    joke = "Error";
+};
